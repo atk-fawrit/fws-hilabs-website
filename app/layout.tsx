5b9ac628ff319af693d4bf3f5048d2e3c3142a86
hilabs-fawrit <kite@dev.fawrit.com>
2026-07-10 17:51:04 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,17 @@ export default function RootLayout({
           href="/images/bg-video.mp4"
           type="video/mp4"
         />
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NQEY8YG2H0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NQEY8YG2H0');
+</script>
+        
       </head>
       <body className="antialiased bg-black text-white font-sans overflow-x-hidden" suppressHydrationWarning={true}>
         <Navigation />
